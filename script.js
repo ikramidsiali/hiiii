938d34da22eed28eb65a41d01172936deadbe155
ikramidsiali <114236412+ikramidsiali@users.noreply.github.com>
2022-11-20 11:54:53 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const color= document.querySelector(".colors");
 const organ= document.querySelector(".organs");
 const fruit= document.querySelector(".fruits");
 const vegetable= document.querySelector(".vegetables");
+const kitchen= document.querySelector(".kitchenTools");
+const Nature= document.querySelector(".nature");
 
 function showCategory(getCategory)  {
     landingPage.classList.add("hide");
@@ -13,15 +15,24 @@ function home()  {
     organ.classList.add("hide");
     color.classList.add("hide");
     fruit.classList.add("hide");
-    vegetable.classList.add("hide"); }
+    vegetable.classList.add("hide");
+    kitchen.classList.add("hide");
+    Nature.classList.add("hide"); }
 
-const red = new Audio();      red.src = "red.opus";
-const orange = new Audio();   orange.src = "orange.opus";
-const yellow = new Audio();   yellow.src = "yellow.opus";
-const green = new Audio();    green.src = "green.opus";
-const blue = new Audio();     blue.src = "blue.opus";
-const indigo = new Audio();   indigo.src = "indigo.opus";
-const violet = new Audio();   violet.src = "violet.opus";
+const red = new Audio();      red.src = "audios/red.opus";
+const pink = new Audio();   pink.src = "audios/pink.opus";
+const gold = new Audio();   gold.src = "audios/gold.opus";
+const yellow = new Audio();    yellow.src = "audios/yellow.opus";
+const orange = new Audio();   orange.src = "audios/orange.opus";
+const green = new Audio();    green.src = "audios/green.opus";
+const blue = new Audio();     blue.src = "audios/blue.opus";
+const purple = new Audio();     purple.src = "audios/purple.opus";
+const brown = new Audio();   brown.src = "audios/brown.opus";
+const white = new Audio();   white.src = "audios/white.opus";
+const silver = new Audio();   silver.src = "audios/silver.opus";
+const gray = new Audio();   gray.src = "audios/gray.opus";
+const black = new Audio();   black.src = "audios/black.opus";
+
 
 const lungs = new Audio();   lungs.src = "audios/lungs.opus";
-const kidney = new Audio();   kidney.src = "kidney.opus";
+const kidney = new Audio();   kidney.src = "audios/kidney.opus";
